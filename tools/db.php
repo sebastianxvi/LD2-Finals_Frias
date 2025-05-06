dfd2807bfcec2b9a4c1464ad0c9202cc600fb0b9
Marvin-Morgan <bossmarvin18@gmail.com>
2025-05-06 23:03:19 +0800

    Update db.php

--- tools/db.php
+++ tools/db.php
@@ -1,18 +1,16 @@
 <?php
-function getDBConnection(){
-    $servername = "switchyard.proxy.rlwy.net";
-    $username = "root";
-    $password = "TzYfrdBKhmUefTAVqmjeatqGWnNqGuwa";
-    $database = "railway";
-    $port = 17607;
+$host = 'switchyard.proxy.rlwy.net';
+$port = 17607;
+$user = 'root';
+$password = 'TzYfrdBKhmUefTAVqmjeatqGWnNqGuwa';
+$dbname = 'railway';
 
+// Create connection
+$conn = new mysqli($host, $user, $password, $dbname, $port);
 
-$connection = new mysqli($servername, $username, $password, $database);
-if($connection->connect_error){
-    die("Error: Failed to connect to MySQL. ".$connection->connect_error);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
-return $connection;
-}
-
+echo "Connected successfully!";
 ?>
